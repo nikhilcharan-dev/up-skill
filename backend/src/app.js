5e98ff745ff6bab396a4df9282d1ab21db265a24
Nikhilcharan-dev <chintufru143@gmail.com>
2026-01-29 18:52:10 +0530

    Client Changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,6 +22,10 @@ app.use(morgan('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
+app.get("/", (req, res) => {
+    res.send("Welcome to Owl Coder!");
+});
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', verifyToken, adminRoutes);

d90d6639474b918345817a8f2f7f3a4d5b752c0d
Nikhilcharan-dev <chintufru143@gmail.com>
2026-01-31 13:41:30 +0530

    Domain CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(helmet({
     crossOriginResourcePolicy: { policy: "cross-origin" },
 }));
-app.use(cors());
+// app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
